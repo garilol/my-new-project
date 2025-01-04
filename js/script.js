a9e26b22e647f4a0e5deb6eefb8810fba6dcbc94
garilol <107126619+garilol@users.noreply.github.com>
2025-01-04 18:28:09 +0100

    Added interactivity and task list to project

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => { console.log('Page loaded!'); });
+document.addEventListener('DOMContentLoaded', () => {
+    const header = document.querySelector('h1');
+    header.addEventListener('click', () => {
+        header.textContent = 'You clicked the title!';
+    });
+});
+
